3c3020b45b124539fe9e4794bcffb5fca2f752fa
Alex <37278122+radayev@users.noreply.github.com>
2026-06-15 14:27:04 -0400

    updates

--- scripts/sidebar-menu.js
+++ scripts/sidebar-menu.js
@@ -1,31 +1,38 @@
+(function () {
+	var MENU_ICONS = [
+		{ href: 'index.jsp',          icon: 'fa-solid fa-book-open' },
+		{ href: 'calendar.jsp',       icon: 'fa-regular fa-calendar-days' },
+		{ href: 'cart.jsp',           icon: 'fa-solid fa-cart-shopping' },
+		{ href: 'login.jsp',          icon: 'fa-solid fa-user' },
+		{ href: 'instructorListing',  icon: 'fa-solid fa-location-dot' }
+	];
 
+	function injectIcons() {
+		var leftMenus = document.querySelectorAll('.leftMenu');
+		if (!leftMenus.length) return;
 
-function injectSidebarMenu() {
-	var leftMenus = document.querySelectorAll('.leftMenu');
-	if (leftMenus.length < 2) return;
+		var navMenu = leftMenus[0];
+		var links = navMenu.querySelectorAll('a');
 
-	var wrapper = document.createElement('div');
-	wrapper.className = 'leftMenu';
-	wrapper.innerHTML = `
-		<aside id="nav_menu-6" class="widget widget_nav_menu">
-			<div class="menu-bergen-ce-clone-container">
-				<ul id="menu-bergen-ce-clone" class="menu">
-					<li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="?categoryId=FC89CDF8">Bergen for Business</a></li>
-					<li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="https://bergen.edu/ce/bergen-for-business/">Bergen for Business</a></li>
-					<li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="https://bergen.edu/ce/courses-programs/">Courses and Programs</a></li>
-					<li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="https://bergen.edu/ce/general-information-and-policies/">General Information and Policies</a></li>
-					<li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="https://bergen.edu/ce/ce-staff-directory/">Staff Directory</a></li>
-					<li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="https://bergen.edu/ce/teaching-opportunities-proposals/">Teaching Opportunities/Proposals</a></li>
-				</ul>
-			</div>
-		</aside>
-	`;
+		for (var i = 0; i < links.length; i++) {
+			var a = links[i];
+			var href = a.getAttribute('href');
 
-	leftMenus[1].parentNode.insertBefore(wrapper, leftMenus[1]);
-}
+			for (var j = 0; j < MENU_ICONS.length; j++) {
+				if (href.indexOf(MENU_ICONS[j].href) !== -1) {
+					var icon = document.createElement('i');
+					icon.className = MENU_ICONS[j].icon;
+					icon.style.marginRight = '0.5em';
+					a.insertBefore(icon, a.firstChild);
+					break;
+				}
+			}
+		}
+	}
 
-if (document.readyState === 'loading') {
-	document.addEventListener('DOMContentLoaded', injectSidebarMenu);
-} else {
-	injectSidebarMenu();
-}
+	if (document.readyState === 'loading') {
+		document.addEventListener('DOMContentLoaded', injectIcons);
+	} else {
+		injectIcons();
+	}
+})();
